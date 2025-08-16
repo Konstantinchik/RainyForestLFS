53679e16de2f461bc6e22fee42cef7a89be7eeb0
Konstantinchik <38495395+Konstantinchik@users.noreply.github.com>
2025-08-16 19:33:24 +0300

    OGSE

---
+++ Assets/OGSE/CustomNPC.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ogse
+{
+
+
+
+    // Новые группы добавлять только в конец
+    public enum NpcGroup
+    {
+        SIDOROVICH,
+        BARMAN,
+        BARMEN,
+        STALKER,
+        NOVICE,
+        SCIENCE,
+        ECOLOG,
+        BANDIT,
+        DOLG,
+        SVOBODA,
+        MILITARY,
+        MONOLIT,
+        CLEARSKY,
+        RENEGAT,
+        ZOMBIED,
+        KILLER
+    }
+
+    public class CustomNPC : MonoBehaviour
+    {
+        bool _isQuestPerson;
+        bool _isZombied;
+        bool _isFriendly;
+        bool _isTrader;
+
+        bool _isAlive;
+
+
+        Animator m_animator;
+
+        /// <summary>
+        /// На правой руке NPC в Prefab к bip01_r_hand добавлен WeaponPoint 
+        /// </summary>
+        [Header("Prefab with all weapons")]
+        [SerializeField]
+        GameObject WeaponAttachPoint;
+        [SerializeField]
+        GameObject[] WeaponList;
+
+
+        void Awake()
+        {
+            m_animator = GetComponent<Animator>();
+        }
+
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+
+        }
+    }
+}
